e36912c241f42bdf8c73f49033fc4b914807f9aa
Gerald Clark <geraldclarkaudio@gmail.com>
2025-01-08 23:17:13 -0600

    added super basic project

---
+++ Source/UE5_Architecture/UE5_Architecture.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_Architecture.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Architecture, "UE5_Architecture" );
