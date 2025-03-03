bb9fd435dd1f624221e80b481b085c41e8e16226
Gerald Clark <geraldclarkaudio@gmail.com>
2025-03-02 22:47:41 -0600

    added ship propel and rotate functionality

--- Source/UE5_Architecture/Private/Ship.cpp
+++ Source/UE5_Architecture/Private/Ship.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Ship.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
 AShip::AShip()
@@ -9,6 +9,20 @@ AShip::AShip()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//to contruct a component, lets use the constructor.. 
+	//set up component as a sub object (UStaticMeshComponent) and name it ShipMesh
+	shipMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
+	//Root Component MUST BE OF THE USCENECOMPONENT... 
+	SetRootComponent(shipMesh); 
+	//make a camera boom object and make it a child of the shipMesh
+	cameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	cameraBoom->SetupAttachment(shipMesh);
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	camera->SetupAttachment(cameraBoom);
+	//***IMPORTANT***
+	cameraBoom->TargetArmLength = 1000.f; // this will only effect the value on a newly constructed object. 
+	//auto possess player 0
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
@@ -16,15 +30,21 @@ void AShip::BeginPlay()
 {
 	Super::BeginPlay();	
 
-	UE_LOG(LogTemp, Warning, TEXT("IMPULSE STRENGTH IS: %f"), impulseStrength);
-
+	//get player controller and store it. 
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController())) {
+		//get sub system/ 
+		if (UEnhancedInputLocalPlayerSubsystem* subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>
+			(PlayerController->GetLocalPlayer())) 
+		{
+			subSystem->AddMappingContext(shipMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
 void AShip::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -32,5 +52,33 @@ void AShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+		
+		//bind actions to a function. 
+		EnhancedInputComponent->BindAction(propelUpAction, ETriggerEvent::Triggered, this, &AShip::PropelUp);
+		EnhancedInputComponent->BindAction(rotateAction, ETriggerEvent::Triggered, this, &AShip::RotateShip);
+	}
+
+}
+
+void AShip::PropelUp(const FInputActionValue& Value) {
+	
+	//get what the value is 
+	bool currentValue = Value.Get<bool>();
+
+	if (currentValue) {
+		//create an impulse. Need to store the impulse in a variable 
+		const FVector worldImpulse = FVector(0, 0, 1) * impulseStrength;
+		const FVector localImpulse = GetActorRotation().RotateVector(worldImpulse);
+		shipMesh->AddImpulse(localImpulse, NAME_None, true);
+	}
+}
+
+void AShip::RotateShip(const FInputActionValue& Value)
+{
+	if (float currentValue = Value.Get<float>()) {
+		const FVector Torque = FVector(1, 0, 0) * currentValue * torqueStrength;
+		shipMesh->AddTorqueInRadians(Torque, NAME_None, true);
+	}
 }
 
--- Source/UE5_Architecture/Public/Ship.h
+++ Source/UE5_Architecture/Public/Ship.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputActionValue.h"
 #include "Ship.generated.h"
+
+
+//forward declaration
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class UE5_ARCHITECTURE_API AShip : public APawn
@@ -14,18 +22,44 @@ class UE5_ARCHITECTURE_API AShip : public APawn
 public:
 	// Sets default values for this pawn's properties
 	AShip();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-private:
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ship | Input")
 	float impulseStrength = 10.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ship | Movement")
+	float torqueStrength = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ship | Graphics")
+	//component type to add * at the end of the type  
+	UStaticMeshComponent* shipMesh;
+	//INPUT STUFF
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ship | Input")
+	UInputMappingContext* shipMappingContext; 
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ship | Input")
+	UInputAction* propelUpAction; // space bar
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ship | Input")
+	UInputAction* rotateAction; // a and D
+
+	//functions 
+	// the value type needs to be const since BindAction method of the EnhancedInputComponent class uses a const FInputActionValue as its first parameter. 
+	void PropelUp(const FInputActionValue& Value);
+	void RotateShip(const FInputActionValue& Value);
+	
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	//camera boom system 
+	USpringArmComponent* cameraBoom;
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* camera;
 };
